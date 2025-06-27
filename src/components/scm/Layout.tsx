08ee9d2658e11672de4f0b21c15559dcbc416e9f
Matthys701006 <jmeviber@gmail.com>
2025-06-27 14:29:22 +0200

    Create Layout.tsx

---
+++ src/components/scm/Layout.tsx
@@ -0,0 +1,31 @@
+
+import { SidebarProvider } from "@/components/ui/sidebar"
+import { Navigation } from "./Navigation"
+import { Header } from "./Header"
+import { PWAInstallPrompt } from "@/components/pwa/PWAInstallPrompt"
+import { OfflineIndicator } from "@/components/pwa/OfflineIndicator"
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <SidebarProvider>
+      <div className="min-h-screen flex w-full bg-background">
+        <Navigation />
+        
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <OfflineIndicator />
+          
+          <main className="flex-1 p-6 overflow-auto">
+            {children}
+          </main>
+        </div>
+        
+        <PWAInstallPrompt />
+      </div>
+    </SidebarProvider>
+  )
+}
